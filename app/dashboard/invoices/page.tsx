f2045618e1f86fe64dd22758eb95baccfe3ef398
mampiononavoahary <hei.zoarisoa@gmail.com>
2024-11-02 20:12:15 +0300

    feat: first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      invoices pages
+    </div>
+  )
+}
+
+export default page
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {JetBrains_Mono} from 'next/font/google';
+import {Lusitana} from 'next/font/google'
+
+export const jetBrainsMono = JetBrains_Mono({subsets:['latin']});
+export const lusitana = Lusitana({weight:['400','700'], subsets:['latin']});
