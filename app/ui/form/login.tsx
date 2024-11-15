bc1c94670f852ad8ad94f2e309785ab6ea1f9227
mampiononavoahary <hei.zoarisoa@gmail.com>
2024-11-15 22:42:28 +0300

    feat: fix bug

--- app/ui/form/login.tsx
+++ app/ui/form/login.tsx
@@ -5,11 +5,11 @@ import { Label } from "@/components/ui/label"
 const Login = () => {
     return (
         <div className="grid w-full max-w-sm items-center gap-4">
-          <Label htmlFor="username">Username</Label>
-          <Input type="username" id="email" placeholder="Username" />
-          <Label htmlFor="password">Password</Label>
-          <Input type="password" id="password" placeholder="Password" />
-          <Button>Log In</Button>
+          <Label htmlFor="nom_d_utilisateur">Nom d'utilisateur</Label>
+          <Input type="nom_d_utilisateur" id="nom_d_utilisateur" placeholder="Nom d'utilisateur" />
+          <Label htmlFor="mot_de_passe">Mot de passe</Label>
+          <Input type="mot_de_passe" id="mot_de_passe" placeholder="Mot de passe" />
+          <Button>Se connecter</Button>
         </div>
       )
 }
