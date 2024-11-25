2ccae197bf6cc666bf7d5bddeafb513af12a23b6
mampiononavoahary <hei.zoarisoa@gmail.com>
2024-11-25 10:31:46 +0300

    fix: fix bug

--- app/lib/logConf.tsx
+++ app/lib/logConf.tsx
@@ -4,7 +4,9 @@ const apiClient = axios.create({
   baseURL: 'https://gestionofstock.onrender.com', // Remplace par l'URL de ton backend
   headers: {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
   },
+  withCredentials:true
 });
 
 export const login = async (username: string, password: string) => {
