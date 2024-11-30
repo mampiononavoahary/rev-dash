d8c96aa4bd652b0b9614aef562034b76f26262c6
mampiononavoahary <hei.zoarisoa@gmail.com>
2024-11-30 09:30:02 +0300

    feat: add initiakizerToken in create transaction page

--- app/dashboard/transactions/create/page.tsx
+++ app/dashboard/transactions/create/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CreateTransaction from '@/app/ui/transactions/create-transaction'
+import InitializeToken from '@/app/lib/initializer';
 
 export const dynamic = 'force-dynamic';
 
 const page = () => {
   return (
     <div>
+      <InitializeToken/>
       <CreateTransaction/>
     </div>
   )
