361368b129d32e40a4b8d11cac7388056ebeda36
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-01-14 14:30:10 +0300

    feat: add new condition on fetch

--- app/ui/transactions/create-transaction.tsx
+++ app/ui/transactions/create-transaction.tsx
@@ -51,9 +51,10 @@ export default function CreateTransaction() {
         const fetchedClients = await getAllClients();
         const fetchedProduits = await getIdAndName();
         const fetchDetailTransaction = await getLastDetailTransaction();
-        setClients(fetchedClients);
-        setProduits(fetchedProduits);
-        setDetailTransaction(fetchDetailTransaction);
+        setClients(fetchedClients || []);
+        setProduits(fetchedProduits || []);
+        setDetailTransaction(fetchDetailTransaction || []);
+        console.log({ fetchedClients, fetchedProduits, fetchDetailTransaction });
       } catch (error) {
         console.error('Erreur lors du chargement des données :', error);
         toast.error('Erreur lors du chargement des données.');
@@ -153,13 +154,15 @@ export default function CreateTransaction() {
             <option value="" disabled>
               Selectionneeer un client
             </option>
-            {clients?.map((client: any) => {
-              return (
+            {clients.length > 0 ? (
+              clients.map((client) => (
                 <option key={client.id_clients} value={client.id_clients}>
                   {client.nom} {client.prenom}
                 </option>
-              );
-            })}
+              ))
+            ) : (
+              <option disabled>Aucun clients</option>
+            )}
           </select>
         </div>
         <div className="flex justify-center">
@@ -197,16 +200,13 @@ export default function CreateTransaction() {
               <option value="" disabled>
                 Clients
               </option>
-              {detailTransaction?.map((detail: any) => {
-                return (
-                  <option
-                    key={detail.id_detail_transaction}
-                    value={detail.id_detail_transaction}
-                  >
-                    {detail.nom} {detail.prenom}
-                  </option>
-                );
-              })}
+
+              {detailTransaction?.map((detail: any) => (
+                <option key={detail.id_detail_transaction} value={detail.id_detail_transaction}>
+                  {detail.nom} {detail.prenom}
+                </option>
+              )) || <option disabled>No transaction details available</option>}
+
             </select>
             <select
               id="id_produit_avec_detail"
@@ -217,16 +217,13 @@ export default function CreateTransaction() {
               <option value="" disabled>
                 Produits
               </option>
-              {produits?.map((produit: any) => {
-                return (
-                  <option
-                    key={produit.id_produit_avec_detail}
-                    value={produit.id_produit_avec_detail}
-                  >
-                    {produit.nom_detail}
-                  </option>
-                );
-              })}
+
+              {produits?.map((produit: any) => (
+                <option key={produit.id_produit_avec_detail} value={produit.id_produit_avec_detail}>
+                  {produit.nom_detail}
+                </option>
+              )) || <option disabled>No products available</option>}
+
             </select>
             <input
               id="quantite"
